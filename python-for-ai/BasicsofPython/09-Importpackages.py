eb1fee2d48d7a971bc9627a8db58138ef7492730
Nihal Parekh <nihalparekh.dev@gmail.com>
2026-08-17 22:17:39 +0530

    Intro to importing package

---
+++ python-for-ai/BasicsofPython/09-Importpackages.py
@@ -0,0 +1,6 @@
+#Think like this 
+#A module is like a toolbox
+#A package is like a garage with multiple toolboxes
+#A fuction is like a specific tool (Hammer, screwdriver, etc)
+#A class is like a blueprint for building tools 
+
